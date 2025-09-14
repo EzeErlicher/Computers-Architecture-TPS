44b821e588574ed7b1bbf5aa04849d233c894fc1
Juan Badariotti <juan.badariotti@mi.unc.edu.ar>
2025-09-14 16:17:48 -0300

    update: basic implementation of alu operations and top

--- TP1-ALU/src/alu.v
+++ TP1-ALU/src/alu.v
@@ -1,15 +1,30 @@
 module alu
 #(
-    parameter in_data_length=8,
-    parameter op_code_length=6             
+    parameter NB_DATA=8,
+    parameter NB_OP=6             
 )
-
 (
-    input wire signed [in_data_length-1:0] operand_1,
-    input wire signed [in_data_length-1:0] operand_2,
-    input wire [op_code_length-1:0] operation_code,
-    output wire signed [in_data_length-1:0] result
+    input wire signed  [NB_DATA-1:0] i_data_a,
+    input wire signed  [NB_DATA-1:0] i_data_b,
+    input wire         [NB_OP-1:0]   i_operation_code,
+    output wire signed [NB_DATA-1:0] o_result
 );
 
+reg signed [NB_DATA-1:0] result;
+
+always @(*) begin
+    case (i_operation_code)
+        6'b100000: result = i_data_a + i_data_b; // ADD
+        6'b100010: result = i_data_a - i_data_b; // SUB
+        6'b100100: result = i_data_a * i_data_b; // AND
+        6'b100101: result = i_data_a / i_data_b; // OR
+        default: result = {NB_DATA{1'b0}};
+    endcase
+end
+
+assign o_result = result;
 
 endmodule
+
+// ToDo: Add more operations
+// ToDo: Bit de Carry? Bit de Zero?
--- TP1-ALU/src/top.v
+++ TP1-ALU/src/top.v
@@ -1,24 +1,45 @@
 module top
 #
 (
-    parameter switches_array_len=8,
-    parameter buttons_array_len=3,
-    parameter led_array_len=8
+    parameter N_SWITCHES=8,
+    parameter N_BUTTONS=3,
+    parameter NB_OPERATIONS=6,
+    parameter N_LEDS=8
 )
 
 (
-    input wire clk,
-    input wire [switches_array_len-1:0] i_sw,
-    input wire [buttons_array_len-1:0] i_button,
-    output wire [led_array_len-1:0] o_led
+    input wire                   i_clk,
+    input wire  [N_SWITCHES-1:0] i_sw,
+    input wire  [N_BUTTONS-1:0]  i_button,
+    output wire [N_LEDS-1:0]     o_led
      
 );
 
-    assign o_led = i_sw;
+    reg signed I_data_a, I_data_b;
+    reg I_operation_code;
 
-/*    always @(posedge clk) 
-    begin
-       reg o_led = i_sw;
+    alu #(
+        .NB_DATA(N_SWITCHES),
+        .NB_OP(NB_OPERATIONS)
+    ) alu_unit (
+        .i_data_a(I_data_a),
+        .i_data_b(I_data_b),
+        .i_operation_code(I_operation_code),
+        .o_result(o_led)
+    );
+
+    always @(posedge i_clk) begin
+        // ToDo: Añadir boton de reset
+
+        if(i_button[0]) begin //Pulsador 1
+            I_data_a <= i_sw;
+        end
+        if (i_button[1]) begin //Pulsador 2
+            I_data_b <= i_sw;
+        end
+        if (i_button[2]) begin //Pulsador 3
+            I_operation_code <= i_sw[NB_OPERATIONS-1:0]; 
+        end
     end
- */
+
 endmodule
