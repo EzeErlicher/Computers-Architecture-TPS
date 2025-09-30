0319652f56541ca1fc6bb5dea1578c19945404e7
Juan Badariotti <juan.badariotti@mi.unc.edu.ar>
2025-09-29 21:12:30 -0300

    TP2: baud rate generator - first implementation

---
+++ TP2-UART/src/baud_gen.v
@@ -0,0 +1,29 @@
+module baud_rate_gen 
+  #( 
+        parameter  NB_COUNTER =   9,
+        parameter  MOD = 163
+    )
+    ( 
+        input           clk,
+        input           reset,
+        output          tick
+    ); 
+
+    reg  [NB_COUNTER-1:0]  r_counter;
+    wire [NB_COUNTER-1:0] r_next;
+
+    always @ (posedge clk)begin
+        if (reset)
+            r_counter <= 0; 
+        else 
+            r_counter <= r_next;
+    end
+
+    // Alcanzó el valor máximo del contador?
+    wire last_value = (r_counter == MOD-1);
+    // Siguiente valor del contador
+    assign r_next = last_value ? 0 : r_counter + 1; 
+    // Tick pulse
+    assign tick = last_value ? 1'b1 : 1'b0; 
+
+endmodule 
