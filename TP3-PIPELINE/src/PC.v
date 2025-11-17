cd305f0d1a2cb36b7a771225aedd4610469fa689
EzeErlicher <103656263+EzeErlicher@users.noreply.github.com>
2025-11-17 19:01:08 -0300

    TP3: program counter added

---
+++ TP3-PIPELINE/src/PC.v
@@ -0,0 +1,33 @@
+module PC #(
+parameter NB_DATA = 32
+)
+(
+input wire i_clk,
+input wire i_reset,
+input wire i_PCwrite,
+input wire [NB_DATA-1:0]i_PC,
+output wire[NB_DATA-1:0]o_PC
+);
+
+
+reg [NB_DATA-1:0] out_PC;
+
+
+always@(posedge i_clk,posedge i_reset) begin
+    
+    if(i_reset) begin
+        out_PC = 0;
+    end
+    
+    else begin
+    
+        if(i_PCwrite)begin
+            out_PC<=i_PC;    
+        end
+    end
+    
+end
+
+assign o_PC=out_PC;
+
+endmodule
