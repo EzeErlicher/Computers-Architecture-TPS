44dec77e66fc550f8db79eb843409520abf1579d
Juan Badariotti <juan.badariotti@mi.unc.edu.ar>
2025-11-19 01:54:12 -0300

    TP3: improved register bank testbench

--- TP3-PIPELINE/tests/register_bank_tb.v
+++ TP3-PIPELINE/tests/register_bank_tb.v
@@ -15,8 +15,9 @@ module register_bank_tb;
     wire [NB_DATA-1:0]   o_register1;
     wire [NB_DATA-1:0]   o_register2;
 
-    reg  [NB_DATA-1:0]   exp0;
-    reg  [NB_DATA-1:0]   exp7;
+    reg  [NB_DATA-1:0]   exp1;
+    reg  [NB_DATA-1:0]   exp2;
+    reg  [NB_REG-1:0]    reg1, reg2;
 
     register_bank #(
         .NB_DATA(NB_DATA),
@@ -36,42 +37,54 @@ module register_bank_tb;
     always #5 i_clk = ~i_clk;
 
     initial begin
-        i_clk = 0; i_reset = 1; i_write_enable = 0;
-        
+
+        i_clk = 0; i_reset = 1;
+        i_write_enable = 0;
         i_write_reg = 0; i_write_data = 0;
         i_read_reg1 = 0; i_read_reg2 = 0;
 
         #10;
         i_reset = 0;
 
-        exp0 = 32'hA5A5A5A5;
-        i_write_reg = 5'd0;
-        i_write_data = exp0;
-        i_write_enable = 1;
+        for (integer i = 0; i < (2**NB_REG)/2; i = i + 1) begin
 
-        #10;
-        $display("[DBG] Internal reg0: %h", dut.registers[0]);
-        i_write_enable = 0; #10;
-        i_read_reg1 = 5'd0; #10;
-        $display("[WRITE] reg0 = %0d | [READ] reg0 = %0d | Status: %s", exp0, o_register1, (o_register1 === exp0) ? "OK" : "ERR");
+            reg1 = $random % (2**NB_REG);
+            reg2 = $random % (2**NB_REG);
 
-        exp7 = 32'hA3A3A3A3;
-        i_write_reg = 5'd7; 
-        i_write_data = exp7;
-        i_write_enable = 1;
+            // Ensure the two registers are different
+            while (reg1 == reg2) begin
+                reg2 = $random % (2**NB_REG);
+            end
 
-        #10;
-        $display("[DBG] Internal reg7: %h", dut.registers[7]);
-        i_write_enable = 0; #10;
-        i_read_reg1 = 5'd7; #10;
+            exp1 = $random & 32'hFFFFFFFF;
+            i_write_reg = reg1;
+            i_write_data = exp1;
+            i_write_enable = 1;
 
-        $display("[WRITE] reg7 = %0d | [READ] reg7 = %0d | Status: %s", exp7, o_register1, (o_register1 === exp7) ? "OK" : "ERR");
+            #10;
+            i_write_enable = 0; #10;
+            i_read_reg1 = reg1; #10;
+            $display("[WRITE] reg%3d = %10d -> [READ] reg%3d = %10d | Status: %s", reg1, exp1, reg1, o_register1, (o_register1 === exp1) ? "OK" : "ERR");
 
-        i_read_reg1 = 5'd0; i_read_reg2 = 5'd7; #10;
-        $display("[DOUBLE READ] reg0 = %0d (Expected: %0d) | Status: %s", o_register1, exp0, (o_register1 === exp0) ? "OK" : "ERR");
-        $display("[DOUBLE READ] reg7 = %0d (Expected: %0d) | Status: %s", o_register2, exp7, (o_register2 === exp7) ? "OK" : "ERR");
+            exp2 = $random & 32'hFFFFFFFF;
+            i_write_reg = reg2; 
+            i_write_data = exp2;
+            i_write_enable = 1;
 
-        #10 $finish;
+            #10;
+            i_write_enable = 0; #10;
+            i_read_reg1 = reg2; #10;
+
+            $display("[WRITE] reg%3d = %10d -> [READ] reg%3d = %10d | Status: %s", reg2, exp2, reg2, o_register1, (o_register1 === exp2) ? "OK" : "ERR");
+
+            i_read_reg1 = reg1; i_read_reg2 = reg2; #10;
+            $display("[DOUBLE READ] reg%3d = %10d (Expected: %10d)  | Status: %s", reg1, o_register1, exp1, (o_register1 === exp1) ? "OK" : "ERR");
+            $display("[DOUBLE READ] reg%3d = %10d (Expected: %10d)  | Status: %s\n", reg2, o_register2, exp2, (o_register2 === exp2) ? "OK" : "ERR");
+
+            #10;
+        end
+        
+        $finish;
     end
 
 endmodule
