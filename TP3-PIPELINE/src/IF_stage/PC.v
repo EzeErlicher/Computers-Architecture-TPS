ff4b882ba10179879ea70e90bab89ee7e237b09f
EzeErlicher <103656263+EzeErlicher@users.noreply.github.com>
2026-02-21 18:08:31 -0300

    TP3: small changes on PC's variable names

--- TP3-PIPELINE/src/IF_stage/PC.v
+++ TP3-PIPELINE/src/IF_stage/PC.v
@@ -1,29 +1,32 @@
 module PC #(
-    parameter NB_DATA = 32
+parameter NB_PC = 6
 )
 (
-    input  wire               i_clk,
-    input  wire               i_reset,
-    input  wire               i_PCwrite,
-    input  wire [NB_DATA-1:0] i_PC,
-    output wire [NB_DATA-1:0] o_PC
+//Inputs
+input wire i_clk,
+input wire i_reset,
+input wire i_PCwrite,
+input wire [NB_PC-1:0]i_PC,
+
+//Outputs
+output wire[NB_PC-1:0]o_PC
 );
 
-reg [NB_DATA-1:0] out_PC;
+reg [NB_PC-1:0] out_PC;
 
-always @(posedge i_clk) begin
-
+always@(posedge i_clk,posedge i_reset) begin
+    
     if(i_reset) begin
-        out_PC = 0;
+        out_PC <= 0;
     end
-
+    
     else begin
-        if(i_PCwrite) begin
-            out_PC <= i_PC;
+        if(i_PCwrite)begin
+            out_PC<=i_PC;    
         end
-    end
+    end    
 end
 
-assign o_PC = out_PC;
+assign o_PC=out_PC;
 
 endmodule
