9efa36e451be8f3d44fe0fb32dd85a8f0168359d
Juan Badariotti <juan.badariotti@mi.unc.edu.ar>
2026-03-17 23:06:42 -0300

    debug unit: rimprove readability for uart_pipeline_interface code

--- TP3-PIPELINE/src/debug_unit/uart_pipeline_interface.v
+++ TP3-PIPELINE/src/debug_unit/uart_pipeline_interface.v
@@ -1,78 +1,78 @@
 module uart_pipeline_interface #(
 
-parameter REG_BANK_WIDTH = 32,
-parameter REG_BANK_ADDR_BITS = 5, // Tamaño por defecto del banco de registros = 32
-parameter DATA_MEM_WIDTH = 32,
-parameter DATA_MEM_ADDR_BITS = 8 ,// Tamaño por defecto de la memoria de datos = 256
-parameter INSTRUCT_MEM_WIDTH = 32,
-parameter INSTRUCT_MEM_ADDR_BITS = 6, // Tamaño por defecto de la memoria de instrucciones = 64
-parameter IF_ID_SIZE = 44, // 1+1+2+32+6+1+1
-parameter ID_EX_SIZE = 156, // 1+1+2+5+4+64+32+32+6+9
-parameter EX_MEM_SIZE = 85, // 1+1+2+5+32+32+6+1+5
-parameter MEM_WB_SIZE = 75 // 1+1+2+5+32+32+1+1
+    parameter REG_BANK_WIDTH     = 32,
+    parameter REG_BANK_ADDR_BITS = 5, // Tamaño por defecto del banco de registros = 32
+    parameter DATA_MEM_WIDTH     = 32,
+    parameter DATA_MEM_ADDR_BITS = 8 ,// Tamaño por defecto de la memoria de datos = 256
+    parameter INSTRUCT_MEM_WIDTH = 32,
+    parameter INSTRUCT_MEM_ADDR_BITS = 6, // Tamaño por defecto de la memoria de instrucciones = 64
+    parameter IF_ID_SIZE  = 44,  // 1+1+2+32+6+1+1
+    parameter ID_EX_SIZE  = 156, // 1+1+2+5+4+64+32+32+6+9
+    parameter EX_MEM_SIZE = 85,  // 1+1+2+5+32+32+6+1+5
+    parameter MEM_WB_SIZE = 75   // 1+1+2+5+32+32+1+1
 )
 (
 //Inputs
 input wire i_clk,
 input wire i_reset,
-input wire [REG_BANK_WIDTH-1:0]i_register_value,
-input wire [DATA_MEM_WIDTH-1:0]i_memory_value,
-input wire [INSTRUCT_MEM_WIDTH-1:0]i_instruct_or_command,
+input wire [REG_BANK_WIDTH-1:0]     i_register_value,
+input wire [DATA_MEM_WIDTH-1:0]     i_memory_value,
+input wire [INSTRUCT_MEM_WIDTH-1:0] i_instruct_or_command,
 input wire i_tx_buffer_done, 
 input wire i_rx_buffer_empty,
 input wire i_program_finished,
-input wire [IF_ID_SIZE-1:0]i_IF_ID_content,
-input wire [ID_EX_SIZE-1:0]i_ID_EX_content,
-input wire [EX_MEM_SIZE-1:0]i_EX_MEM_content,
-input wire [MEM_WB_SIZE-1:0]i_MEM_WB_content,
+input wire [IF_ID_SIZE-1:0]  i_IF_ID_content,
+input wire [ID_EX_SIZE-1:0]  i_ID_EX_content,
+input wire [EX_MEM_SIZE-1:0] i_EX_MEM_content,
+input wire [MEM_WB_SIZE-1:0] i_MEM_WB_content,
 
 //Outputs
-output wire [REG_BANK_ADDR_BITS-1:0]o_register_address,
-output wire [DATA_MEM_ADDR_BITS-1:0]o_memory_address,
+output wire [REG_BANK_ADDR_BITS-1:0] o_register_address,
+output wire [DATA_MEM_ADDR_BITS-1:0] o_memory_address,
 output wire o_instruct_mem_write_enable,
-output wire [INSTRUCT_MEM_WIDTH-1:0]o_instruct_to_write,
-output wire [INSTRUCT_MEM_ADDR_BITS-1:0]o_instruct_to_write_addr,
-output wire [INSTRUCT_MEM_WIDTH-1:0]o_pipeline_info,
+output wire [INSTRUCT_MEM_WIDTH-1:0]     o_instruct_to_write,
+output wire [INSTRUCT_MEM_ADDR_BITS-1:0] o_instruct_to_write_addr,
+output wire [INSTRUCT_MEM_WIDTH-1:0]     o_pipeline_info,
 output wire o_rx_buffer_start,
-output wire [1:0]o_pipeline_exec_mode,
+output wire [1:0] o_pipeline_exec_mode,
 output wire o_execute_instruct
 );
 
 //States (one hot encoding)
-localparam WAIT_FOR_COMMAND = 9'b000000001;
-localparam INTERPRET_COMMAND = 9'b000000010;
-localparam RECEIVE_INSTRUCTS = 9'b000000100;
+localparam WAIT_FOR_COMMAND     = 9'b000000001;
+localparam INTERPRET_COMMAND    = 9'b000000010;
+localparam RECEIVE_INSTRUCTS    = 9'b000000100;
 localparam PROGRAM_INSTRUCT_MEM = 9'b000001000;
-localparam SEND_REGISTERS = 9'b000010000;
-localparam SEND_LATCHES = 9'b000100000;
-localparam SEND_DATA_MEM = 9'b001000000;
-localparam RUN_CONTINUOS = 9'b010000000;
-localparam RUN_STEPWISE = 9'b100000000;
+localparam SEND_REGISTERS       = 9'b000010000;
+localparam SEND_LATCHES         = 9'b000100000;
+localparam SEND_DATA_MEM        = 9'b001000000;
+localparam RUN_CONTINUOS        = 9'b010000000;
+localparam RUN_STEPWISE         = 9'b100000000;
 
 //Commands
-localparam [INSTRUCT_MEM_WIDTH-1:0] start_continuos = "cont";
-localparam [INSTRUCT_MEM_WIDTH-1:0] start_stepwise = "step";
-localparam [INSTRUCT_MEM_WIDTH-1:0] execute_an_instruct = "exin";
-localparam [INSTRUCT_MEM_WIDTH-1:0] receive_instructions = "rins";
-localparam [INSTRUCT_MEM_WIDTH-1:0] fetch_pipeline_data = "fpip";
-localparam [INSTRUCT_MEM_WIDTH-1:0] instructs_eof = "ieof";
+localparam [INSTRUCT_MEM_WIDTH-1:0] run_continuous           = 8'b00000001;
+localparam [INSTRUCT_MEM_WIDTH-1:0] run_stepwise             = 8'b00000010;
+localparam [INSTRUCT_MEM_WIDTH-1:0] execute_next_instruction = 8'b00000011;
+localparam [INSTRUCT_MEM_WIDTH-1:0] receive_instructions     = 8'b00000100;
+localparam [INSTRUCT_MEM_WIDTH-1:0] fetch_pipeline_data      = 8'b00000101;
+localparam [INSTRUCT_MEM_WIDTH-1:0] instructs_eof            = 8'b00000110;
 
 // Auxiliar variables
 reg [INSTRUCT_MEM_WIDTH-1:0] instructions [2**INSTRUCT_MEM_ADDR_BITS-1:0];
-reg [8:0]state;
+reg [8:0] state;
 reg [INSTRUCT_MEM_ADDR_BITS-1:0] inst_counter;
-reg [INSTRUCT_MEM_WIDTH-1:0] instruct_to_write;
-reg [REG_BANK_ADDR_BITS:0] register_address;
-reg [DATA_MEM_ADDR_BITS:0] memory_address;
+reg [INSTRUCT_MEM_WIDTH-1:0]     instruct_to_write;
+reg [REG_BANK_ADDR_BITS:0]       register_address;
+reg [DATA_MEM_ADDR_BITS:0]       memory_address;
 reg instruct_mem_write_enable;
 reg send_mem_index_or_value; // 0: index, 1:value
-reg [2:0] latches_sent_counter;
-reg [7:0] latch_words_sent;
+reg [2:0]             latches_sent_counter;
+reg [7:0]             latch_words_sent;
 reg [ID_EX_SIZE+31:0] latches_info_array [3:0];
-reg [31:0] current_latch_size;
+reg [31:0]            current_latch_size;
 reg [INSTRUCT_MEM_WIDTH-1:0] pipeline_info;
-reg rx_buffer_start;
-reg [1:0] pipeline_exec_mode;
+reg                          rx_buffer_start;
+reg [1:0]                    pipeline_exec_mode;
 reg execute_instruct;
 reg send_pipeline_data_flag;
 reg return_to_run_stepwise; 
@@ -125,7 +125,7 @@ always @(posedge i_clk,posedge i_reset)begin
                     state <= SEND_REGISTERS;
                 end
             
-                else if (instructions[0] == start_continuos)begin
+                else if (instructions[0] == run_continuous)begin
                     state <= RUN_CONTINUOS;
                     pipeline_exec_mode <= 2'b01;
                 end
@@ -273,7 +273,7 @@ always @(posedge i_clk,posedge i_reset)begin
                     return_to_run_stepwise <=1'b1;
                 end
                 
-                else if(i_tx_buffer_done == 1'b1 && i_instruct_or_command == execute_an_instruct )begin
+                else if(i_tx_buffer_done == 1'b1 && i_instruct_or_command == execute_next_instruction )begin
                      execute_instruct <= 1'b1;
                      send_pipeline_data_flag <= 1'b1;                     
                 end
@@ -296,15 +296,15 @@ always @(*)begin
     endcase
 end
 
-assign o_instruct_to_write = instruct_to_write; 
-assign o_instruct_to_write_addr = inst_counter;
-assign o_register_address = register_address[REG_BANK_ADDR_BITS-1:0];
-assign o_memory_address = memory_address[DATA_MEM_ADDR_BITS-1:0];
+assign o_instruct_to_write         = instruct_to_write; 
+assign o_instruct_to_write_addr    = inst_counter;
+assign o_register_address          = register_address[REG_BANK_ADDR_BITS-1:0];
+assign o_memory_address            = memory_address[DATA_MEM_ADDR_BITS-1:0];
 assign o_instruct_mem_write_enable = instruct_mem_write_enable;
-assign o_pipeline_info = pipeline_info;
-assign o_rx_buffer_start = rx_buffer_start;
-assign o_pipeline_exec_mode = pipeline_exec_mode;
-assign o_execute_instruct = execute_instruct;
+assign o_pipeline_info             = pipeline_info;
+assign o_rx_buffer_start           = rx_buffer_start;
+assign o_pipeline_exec_mode        = pipeline_exec_mode;
+assign o_execute_instruct          = execute_instruct;
 
 endmodule
 
