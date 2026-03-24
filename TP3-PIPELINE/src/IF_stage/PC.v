16773f274edf0fd92d5175dd7471379c5ed10347
EzeErlicher <103656263+EzeErlicher@users.noreply.github.com>
2026-03-24 11:00:35 -0300

    IF stage: PC.v modified

--- TP3-PIPELINE/src/IF_stage/PC.v
+++ TP3-PIPELINE/src/IF_stage/PC.v
@@ -1,28 +1,33 @@
 module PC #(
-    parameter NB_PC = 10
+parameter NB_PC = 10
 )
 (
 //Inputs
-input wire             i_clk,
-input wire             i_reset,
-input wire             i_PC_enable,
-input wire [NB_PC-1:0] i_address,
+input wire i_clk,
+input wire i_reset,
+input wire i_PCWrite,
+input wire [NB_PC-1:0]i_PC,
+input wire [1:0]i_pipeline_exec_mode,
+input wire i_execute_instruct,
 
 //Outputs
-output wire [NB_PC-1:0] o_PC
+output wire[NB_PC-1:0]o_PC
 );
+
+localparam CONT_MODE = 2'b01;
+localparam STEP_MODE = 2'b11;
 
 reg [NB_PC-1:0] out_PC;
 
-always@(posedge i_clk) begin
+always@(posedge i_clk,posedge i_reset) begin
     
     if(i_reset) begin
         out_PC <= 0;
     end
     
     else begin
-        if(i_PC_enable)begin
-            out_PC<=i_address;
+        if((i_pipeline_exec_mode == CONT_MODE || (i_pipeline_exec_mode == STEP_MODE && i_execute_instruct)) && i_PCWrite)begin
+            out_PC<=i_PC;    
         end
     end    
 end
