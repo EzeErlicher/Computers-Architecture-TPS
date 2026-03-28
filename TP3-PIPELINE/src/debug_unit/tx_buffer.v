476d6a1dcfd18b7ca4ea849c8a4e421634a90122
EzeErlicher <103656263+EzeErlicher@users.noreply.github.com>
2026-03-28 17:23:03 -0300

    debug_unit: TX and RX buffer updated

--- TP3-PIPELINE/src/debug_unit/tx_buffer.v
+++ TP3-PIPELINE/src/debug_unit/tx_buffer.v
@@ -13,16 +13,19 @@ input wire [INSTRUCT_MEM_WIDTH-1:0]i_pipeline_info,
 
 //Outputs
 output wire o_tx_buffer_empty,
+output wire o_tx_start,
 output wire [7:0]o_tx_data
 );
 
 reg [INSTRUCT_MEM_WIDTH-1:0]tx_buffer_data;
+reg tx_start;
 reg [7:0]byte_to_send;
 reg tx_buffer_empty;
 reg [2:0]sent_bytes_counter;
 
 always @(posedge i_clk,posedge i_reset)begin
-
+    tx_start <= 1'b0;  
+    
     if (i_reset)begin
         tx_buffer_data <= 0;
         byte_to_send <= 1'b0;
@@ -34,7 +37,8 @@ always @(posedge i_clk,posedge i_reset)begin
         tx_buffer_empty <= 1'b0;
         tx_buffer_data <= i_pipeline_info;
         byte_to_send <= i_pipeline_info[0+:8]; 
-        sent_bytes_counter <= 3'b001;              
+        sent_bytes_counter <= 3'b001;
+        tx_start <= 1'b1;              
     end
     
     else begin
@@ -48,7 +52,8 @@ always @(posedge i_clk,posedge i_reset)begin
             
             else begin
                 byte_to_send <= tx_buffer_data[8*sent_bytes_counter+:8]; 
-                sent_bytes_counter <= sent_bytes_counter + 1; 
+                sent_bytes_counter <= sent_bytes_counter + 1;
+                tx_start <= 1'b1; 
             end
         end
     end  
@@ -56,6 +61,7 @@ end
 
 assign o_tx_buffer_empty = tx_buffer_empty;
 assign o_tx_data = byte_to_send;
+assign o_tx_start = tx_start;
 
 endmodule
 
