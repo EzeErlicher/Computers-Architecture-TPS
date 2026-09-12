56a628ccf3e84f637babde487dfe69ce1b854cf3
EzeErlicher <ezequiel.erlicher@unc.edu.ar>
2026-09-11 22:13:45 -0300

    IF_ID_latch: changed IF_ID_data assignment logic

--- TP3-PIPELINE/src/latches/IF_ID_latch.v
+++ TP3-PIPELINE/src/latches/IF_ID_latch.v
@@ -43,29 +43,22 @@ always @(posedge i_clk or posedge i_reset) begin
     if (i_reset || i_IF_flush) begin
         instruction <= {NB_INSTRUCT{1'b0}};
         PC <= {NB_PC{1'b0}};
-        EOF_flag <= 1'b0;
-        IF_ID_data[PC_LSB +: NB_PC]< =0;
-        IF_ID_data[INSTR_LSB +: NB_INSTRUCT]<= 0;
-        IF_ID_data[PIPEMODE_LSB +: 2] <= 0;
-        IF_ID_data[EXEC_INST_BIT] <= 0;
-        IF_ID_data[EOF_BIT]  <= 0;
-        // everything in IF_ID_data is zeroed except for i_IF_flush and i_IF_ID_write
     end
     
     else if (i_IF_ID_write && (i_pipeline_mode == CONT_MODE || (i_pipeline_mode == STEP_MODE && i_execute_instruct)) )begin
-        
-        IF_ID_data[0] <= i_IF_flush;
-        IF_ID_data[1] <= i_IF_ID_write;
-        IF_ID_data[PC_LSB +: NB_PC] <= i_PC;
-        IF_ID_data[INSTR_LSB +: NB_INSTRUCT] <= i_instruction;
-        IF_ID_data[PIPEMODE_LSB +: 2] <= i_pipeline_mode;
-        IF_ID_data[EXEC_INST_BIT] <= i_execute_instruct;
-        IF_ID_data[EOF_BIT] <= (i_instruction == instructs_eof);
-        
+
         instruction <= i_instruction;
         PC          <= i_PC;
         EOF_flag    <= (i_instruction == instructs_eof);
     end
+
+    IF_ID_data[0] <= i_IF_flush;
+    IF_ID_data[1] <= i_IF_ID_write;
+    IF_ID_data[PC_LSB +: NB_PC] <= i_PC;
+    IF_ID_data[INSTR_LSB +: NB_INSTRUCT] <= i_instruction;
+    IF_ID_data[PIPEMODE_LSB +: 2] <= i_pipeline_mode;
+    IF_ID_data[EXEC_INST_BIT] <= i_execute_instruct;
+    IF_ID_data[EOF_BIT] <= (i_instruction == instructs_eof);
       
 end
 
